893499ec1fe7e15eb6dd4fd7893162b01c0e3003
Joel Varghese Siby <joelhehehe1234@gmail.com>
2026-03-19 18:58:13 +0530

    Time: 6 ms (50.17%) | Memory: 197.9 MB (36.24%) - LeetSync

---
+++ 2196-reverse-nodes-in-even-length-groups/reverse-nodes-in-even-length-groups.java
@@ -0,0 +1,57 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseEvenLengthGroups(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+        ListNode curr = dummy.next;
+        int size = 1;
+      
+        while(curr !=null){
+        ListNode temp = curr;
+          int count =0;
+          while(temp != null && count < size){
+            temp = temp.next;
+            count++;
+          }
+          if (count % 2 == 0) {
+                ListNode prevNode = temp;
+                ListNode node = curr;
+
+                for (int i = 0; i < count; i++) {
+                    ListNode next = node.next;
+                    node.next = prevNode;
+                    prevNode = node;
+                    node = next;
+                }
+
+                ListNode newHead = prevNode;
+                prev.next = newHead;
+
+                prev = curr;
+                curr = temp;
+            } 
+            else {
+                for (int i = 0; i < count; i++) {
+                    prev = curr;
+                    curr = curr.next;
+                }
+            }
+
+            size++;
+        }
+
+        return dummy.next;
+    }
+
+    
+}
